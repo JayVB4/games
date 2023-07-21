c6112f01c4e69e3effdda9ab42bfcd2c2d4b7968
kingsk0 <71212181+kingsk0@users.noreply.github.com>
2023-07-21 16:11:16 +0530

    added text box for numbers

--- minesweeper/Assets/scripts/randombomb.cs
+++ minesweeper/Assets/scripts/randombomb.cs
@@ -1,21 +1,45 @@
 using System.Linq;
 using UnityEngine;
+using TMPro;
+
 public class randombomb : MonoBehaviour
 {
     public GameObject bomb;
+    public GameObject txt;
+    public TMP_Text t;
     public GameObject[] items;
     int temp;
-    int[] randomnum={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+    int[] grid = new int[100];
+    int[] randomnum = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+
     void Start()
     {
+        for (int i = 0; i < 100; i++)
+        {
+            grid[i] = 0;
+        }
         for (int i = 0; i < 10; i++)
         {
             do
             {
-                temp = Random.Range(0,items.Length);        
-            }while(randomnum.Contains(temp));
+                temp = Random.Range(0, items.Length);
+            } while (randomnum.Contains(temp));
             GameObject clone = Instantiate(bomb,items[temp].transform.position,Quaternion.identity);
             randomnum[i] = temp;
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            if(randomnum.Contains(i)){
+                grid[i+1]++;
+            }
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            t.text = grid[i].ToString();
+            if (!(randomnum.Contains(i)))
+            {
+                GameObject clone = Instantiate(txt,items[i].transform.position,txt.transform.rotation);
+            }
         }
     }
 }
